3ef90a1dbd04bf3a81e56d73ca89638394a55f8f
nitabarman <nitabarman@yahoo.com>
2017-02-22 14:51:27 +0600

    Delete baseurl.php

--- baseurl.php
+++
@@ -1,7 +0,0 @@
-<?php
-	 
-	$BaseUrl = '/trackmyvisitor/api/v1/'; 
-	$SiteBaseUrl = '/trackmyvisitor/';
-	$ImageUploadUrl = 'http://trackmyvisitor/'; 
-	$ImageBaseUrl = "http://" . gethostname() . '/trackmyvisitor/images/'; 
-?>
